7f3486579ca2ee1fe06f7df7c17454f78b18df22
hafsafatima <fatimahafsa365@gmail.com>
2026-03-28 16:51:32 +0500

    Made addition function

--- index3.cpp
+++ index3.cpp
@@ -1,8 +1,20 @@
 
 #include <iostream>
 using namespace std;
+int add(int, int);
+
 int main()
 {
-
+    int a, b;
     cout << "The third file :)" << endl;
+    cout << "Lets commit something new" << endl;
+    cout << "For addition give inputs: " << endl;
+    cin >> a;
+    cin >> b;
+    cout << "Addition result: " << a << "+" << b << " = " << add(a, b) << endl;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
